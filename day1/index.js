4934cc2baa06e360a82a60379b20f084205635ef
Wozacosta <srabah-m@student.42.fr>
2016-12-09 10:33:39 +0100

    move script in index.js

--- day1/index.js
+++ day1/index.js
@@ -1,3 +1,19 @@
-/**
- * Created by woza on 12/9/16.
- */
+function playSound(e){
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if(!audio) return; // stop the function from running all together
+  audio.currentTime = 0; // rewind to the start
+  audio.play();
+  key.classList.add("playing");
+  //setTimeout((function(key){ return function() { key.classList.remove("playing")};})(key),150);
+}
+
+
+function removeTransition(e){
+  if (e.propertyName !== "transform") return; // skip it if not transform
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+window.addEventListener('keydown', playSound);
