f879fee8a6bf6ed3a626bf926e799f758d53b76e
Wozacosta <srabah-m@student.42.fr>
2016-12-09 10:44:28 +0100

    fix keyrepeat bug

--- day1/index.js
+++ day1/index.js
@@ -2,6 +2,9 @@ function playSound(e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if(!audio) return; // stop the function from running all together
+  //console.log(audio.currentTime);
+  if (audio.currentTime > 0 && audio.currentTime < 0.01) // fix bug with keyrepeat
+    return ;
   audio.currentTime = 0; // rewind to the start
   audio.play();
   key.classList.add("playing");
