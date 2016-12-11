3ceaa14c7f97a12c16881f88a00c5b54c4352944
Wozacosta <srabah-m@student.42.fr>
2016-12-11 16:50:37 +0100

    opperational clock

---
+++ day2/index.js
@@ -0,0 +1,20 @@
+const secondHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand =  document.querySelector(".hour-hand");
+function setDate(){
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+  const secondsDegrees = ((seconds / 60) * 360) + 90; //+ 90 because of css default offset to get the hand upright (and not horizontal)
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  const hoursDegrees = ((hours / 24) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  console.log(seconds);
+  console.log(minutes);
+  console.log(hours);
+}
+
+setInterval(setDate, 1000);
